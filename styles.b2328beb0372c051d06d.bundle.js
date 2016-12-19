188c4c6cef9784472b11578f7bd9fe7b728aa0d3
Mario Dario Lopez Marquez <mario.lopez@yuxipacific.com>
2016-12-19 14:40:36 -0500

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{330:function(n,o){},599:function(n,o,c){n.exports=c(330)}},[599]);
+webpackJsonp([1,2],{422:function(n,o){},982:function(n,o,c){n.exports=c(422)}},[982]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
